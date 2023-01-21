2c251daf486542cadef42f85f47903183ec030c3
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2023-01-21 16:07:13 +0530

    commit

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -35,7 +35,7 @@ const dataObj = JSON.parse(data);
 
 const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
-console.log(slugs);
+// console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
